9c39f3bdd61256d8e9a0a0a82fe5947cfad02011
Hedi-Slm <hedi.slm42@gmail.com>
2025-03-20 19:36:00 +0100

    Created User model

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.IntegerField(validators=[MinValueValidator(15)], null=True)
+    can_be_contacted = models.BooleanField(default=False)
+    can_data_be_shared = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
