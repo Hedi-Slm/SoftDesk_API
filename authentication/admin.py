dad84e3568be6121982009acca8292a710014633
Hedi-Slm <hedi.slm42@gmail.com>
2025-03-20 19:35:32 +0100

    Added User table to django admin panel

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Define fields to display in the Django Admin panel
+    list_display = ('id', 'email', 'username', 'is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('id',)
+
+    # Define fieldsets for detailed view in admin
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Define fields for user creation in admin
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}),
+    )
+
+    # Ensure email is used for login in admin
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
