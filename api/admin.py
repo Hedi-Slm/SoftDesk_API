14dc512fb4149033f54bc059b25fba96d91b948d
Hedi-Slm <hedi.slm42@gmail.com>
2025-03-26 23:30:58 +0100

    added Project table to django admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Contributor, Issue, Comment
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'type', 'author', 'created_time')
+    search_fields = ('title', 'author__username')
+    list_filter = ('type', 'created_time')
