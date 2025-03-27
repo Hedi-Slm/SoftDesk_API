4e6d57b9cd2ff6b89c53982b6c1c23586d5d75ad
Hedi-Slm <hedi.slm42@gmail.com>
2025-03-27 07:34:22 +0100

    Added filter backend to settings

--- softdesk_api/settings.py
+++ softdesk_api/settings.py
@@ -132,10 +132,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ),
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
