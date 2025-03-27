be8aa17588fab481fb30ebb0b89c02e436f00fd9
Hedi-Slm <hedi.slm42@gmail.com>
2025-03-27 07:34:46 +0100

    Added issue url to router

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from .views import ProjectViewSet
+from .views import ProjectViewSet, IssueViewSet
 
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet, basename='projects')
+router.register('issues', IssueViewSet, basename='issues')
 
 
 urlpatterns = [
