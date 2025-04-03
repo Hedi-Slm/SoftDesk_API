031052affdd7db2ad09916e616c483a83f4b488d
Hedi-Slm <hedi.slm42@gmail.com>
2025-04-03 19:55:33 +0200

    Added CommentViewSet urls

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework_nested import routers
-from .views import ProjectViewSet, IssueViewSet
+from .views import ProjectViewSet, IssueViewSet, CommentViewSet
 
 
 router = routers.DefaultRouter()
 router.register('projects', ProjectViewSet, basename='projects')
 router.register('issues', IssueViewSet, basename='issues')
+router.register('comments', CommentViewSet, basename='comments')
 
 
 urlpatterns = [
